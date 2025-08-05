46b6c73193fffe107c4acf2f368823099fce3f9f
James Royer <jchristopherroyer@gmail.com>
2025-08-05 14:37:28 -0400

    Update notes.py

--- Notes/notes.py
+++ Notes/notes.py
@@ -1,66 +1,23 @@
-# normal access w/property decorator
-class Rectangle:
-    def __init__(self, width, height):
-        # "_" before the attribute indicates that it is
-        # private and can't be accessed outside of 
-        self._width = width
-        self._height = height
-
-
-# begin "getter" methods: specifies the format when
-# the values are called
-
-    @property
-    def width(self):
-        return f"{self._width:.1f}cm"
-
-    @property
-    def height(self):
-        return f"{self._height:.1f}cm"
+# a decorator function will take a function as an argument
+def add_sprinkles(func):
+    # inner "wrapper" function that calls the original function
+    def wrapper(*args, **kwargs):
+        print("Adding sprinkles...")
+        func(*args, **kwargs)
+    return wrapper
     
-# end "getter" methods    
+def add_fudge(func):
+    # wrapper function must take boths *args and **kwargs
+    # to prevent type errors
+    def wrapper(*args, **kwargs):
+        print("Adding fudge...")
+        # same with the func call
+        func(*args, **kwargs)
+    return wrapper
 
-# begin "setter" methods: define how to set (write) a
-# value for an attribute
+@add_sprinkles
+@add_fudge
+def get_ice_cream(flavor):
+    print(f"Your ice cream ({flavor}) is here!")
 
-    @width.setter
-    def width(self, new_width):
-        if new_width > 0:
-            self._width = new_width
-        else:
-            print("Width must be > 0")
-
-    @height.setter
-    def height(self, new_height):
-        if new_height > 0:
-            self._width = new_height
-        else:
-            print("Height must be > 0")
-
-# end "setter" methods
-
-# begin "deleter" methods:
-
-    @width.deleter
-    def width(self):
-        del self._width
-        print("Width has been deleted")
-
-    @height.deleter
-    def height(self):
-        del self._width
-        print("Height has been deleted")
-
-rectangle = Rectangle(3, 4)
-
-
-# still "works" but returns a warning that these are
-# protected class members
-print(rectangle.width) #3
-print(rectangle.height) #4
-
-
-del rectangle.width
-# no longer works: there is no "._width" element for the
-# rectangle object
-print(rectangle.width) #3
+get_ice_cream("vanilla")
