e6ef1501519728a945f6eccd314d266b947e756c
James Royer <jchristopherroyer@gmail.com>
2025-08-06 17:38:04 -0400

    Update notes.py

--- Notes/notes.py
+++ Notes/notes.py
@@ -1,23 +1,9 @@
-# a decorator function will take a function as an argument
-def add_sprinkles(func):
-    # inner "wrapper" function that calls the original function
-    def wrapper(*args, **kwargs):
-        print("Adding sprinkles...")
-        func(*args, **kwargs)
-    return wrapper
-    
-def add_fudge(func):
-    # wrapper function must take boths *args and **kwargs
-    # to prevent type errors
-    def wrapper(*args, **kwargs):
-        print("Adding fudge...")
-        # same with the func call
-        func(*args, **kwargs)
-    return wrapper
-
-@add_sprinkles
-@add_fudge
-def get_ice_cream(flavor):
-    print(f"Your ice cream ({flavor}) is here!")
-
-get_ice_cream("vanilla")
+try:    # always executes
+    number = int(input("Enter a number to divide 1 by: "))
+    print(1 / number)
+except TypeError:
+    print("That is not a number")
+except ZeroDivisionError:
+    print("Cannot divide by 0")
+except Exception: # catch all that detects ANY exception
+    print("Complete")
